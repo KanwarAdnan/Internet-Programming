abf045eacaf8929abd36c3b7bc092a0ce7ef5dde
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2023-07-11 12:39:47 +0500

    LinkedList (Day 1)

---
+++ List.java
@@ -0,0 +1,75 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    private Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.getNext();
+        }
+        System.out.println();
+    }
+}
+
+
+public class List
+{
+	public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        // Insert elements into the list
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.insert(20);
+
+        // Display the contents of the list
+        list.display();
+	}
+}
